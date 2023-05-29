ab1930fde94e32a200cd8c2f8d610a0fa16aa9ec
Chrisvasa <Christophervasankari@gmail.com>
2023-05-29 13:34:22 +0200

    Added project files

---
+++ WeatherAPI.Tests/WeatherForecastTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WeatherAPI.Tests
+{
+    public class WeatherForecastTests
+    {
+        //[Fact]
+        // Method here
+    }
+}
---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,36 @@
+namespace WeatherAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            // API GOES HERE xD
+
+            app.UseCors();
+            app.Run();
+        }
+    }
+}
