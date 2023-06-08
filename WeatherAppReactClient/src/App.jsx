d9b28e343b5db9a2c568c4000c92759e23ee57eb
Wettergrund <jonas.wettergrund@gmail.com>
2023-06-08 10:30:24 +0200

    Added base UI and dummy info

--- WeatherAppReactClient/src/App.jsx
+++ WeatherAppReactClient/src/App.jsx
@@ -1,34 +1,68 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components';
+import Weather from './Weather';
+
+const MainConatiner = styled.div`
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+  background: #302f3d;
+  height: 100vh;
+  width: 100vw;
+  padding-top: 2rem; 
+
+`;
+
+const WeatherCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #363546;
+  color: #b4b4bf;
+  
+  width: 15rem;
+  height: 15rem;
+  
+  border-radius: .3rem;
+  font-family: Roboto, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+
+  box-shadow: 0 10px 20px -15px black; 
+
+  & h2{
+    margin: 0;
+
+    font-size: 1rem;
+
+  }
+
+  & .temp{
+    
+    font-size: 3rem;
+    margin: 0;
+  }
+  & .weather{
+    font-size: 1rem;
+    margin: 0;
+  }
+
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MainConatiner>
+      {/* For each city, generate card, dummy info below */}
+      <Weather city={"Stockholm"} temp={"15°C"} weather={"Sunny"}/>
+      <Weather city={"Oslo"} temp={"10°C"} weather={"Rainy"}/>
+
+
+
+    </MainConatiner>
   )
 }
 
