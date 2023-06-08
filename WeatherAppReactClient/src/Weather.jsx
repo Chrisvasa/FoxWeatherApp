e49fe2ad3d1063ce1b6dd01023079cec143a82af
Ilyas Kaya <113895247+AkiVonAkira@users.noreply.github.com>
2023-06-08 15:16:16 +0200

    dropdown and styling
    added dropdown and changed some of the styling to match our early figma mockup

--- WeatherAppReactClient/src/Weather.jsx
+++ WeatherAppReactClient/src/Weather.jsx
@@ -1,50 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 const WeatherCard = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: #363546;
-  color: #b4b4bf;
-  
+  background: #0086a5;
+  color: #ffffff;
+  flex-grow: 3;
+
   width: 15rem;
   height: 15rem;
-  
-  border-radius: .3rem;
+
+  border-radius: 0.45rem;
   font-family: Roboto, Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
 
-  box-shadow: 0 10px 20px -15px black; 
+  box-shadow: 0 10px 20px -15px black;
 
-  & h2{
-    margin: 0;
-
-    font-size: 1rem;
-
+  & h2 {
+    font-size: 1.5rem;
+    font-weight: 600;
   }
 
-  & .temp{
-    
+  & .temp {
     font-size: 3rem;
-    margin: 0;
+    font-weight: 700;
+    color: #ef8354;
   }
-  & .weather{
-    font-size: 1rem;
-    margin: 0;
+  & .weather {
+    font-size: 1.25rem;
   }
-
 `;
 
 const Weather = ({ city, temp, weather }) => {
   return (
     <WeatherCard>
-        <h2>{city}</h2>
-        <p className='temp'>{temp}</p>
-        <p className='weather'>{weather}</p>
+      <h2>{city}</h2>
+      <p className="temp">{temp}</p>
+      <p className="weather">{weather}</p>
     </WeatherCard>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
