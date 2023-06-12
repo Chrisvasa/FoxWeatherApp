34455b6675062c9545bbca22ec069591674942d2
unknown <rohnson95@gmail.com>
2023-06-12 10:57:05 +0200

    added healthcheck testing

--- WeatherAPI.Tests/WeatherForecastTests.cs
+++ WeatherAPI.Tests/WeatherForecastTests.cs
@@ -20,5 +20,22 @@ namespace WeatherAPI.Tests
             // Assert
             Assert.Equal(expected, actual.StatusCode);
         }
+        [Theory]
+        [InlineData("/api/healthcheck", HttpStatusCode.OK)]
+        [InlineData("/api/healthcheck", HttpStatusCode.InternalServerError)]
+        public async Task ApiHealthCheckShouldReturnOK(string endpoint, HttpStatusCode expected)
+        {
+            // Arrange
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            // Act
+            HttpResponseMessage actual = await client.GetAsync(endpoint);
+            // Assert
+            Assert.True(actual.StatusCode == expected);
+        }
     }
+
+
+
+
 }
