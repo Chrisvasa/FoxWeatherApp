22e15eeb281c6438f18ed4ab388bbe26d6319534
L-stal <stalenhag.leo@gmail.com>
2023-06-12 09:26:26 +0200

    Tests passed when called with correct data

---
+++ WeatherAPI.Tests/ApiCallCounter.cs
@@ -0,0 +1,22 @@
+﻿namespace WeatherAPI.Tests
+{
+        public class ApiCallCounter
+        {
+            private int count;
+
+            public ApiCallCounter()
+            {
+                count = 0;
+            }
+
+            public void Increment()
+            {
+                count++;
+            }
+
+            public int GetCount()
+            {
+                return count;
+            }
+        }
+}
